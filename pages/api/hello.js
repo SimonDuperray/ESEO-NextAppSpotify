b0ef668bab697e083d9708f52979e8b9db3f356c
Simon Duperray <simon.duperray@reseau.eseo.fr>
2022-11-09 18:59:42 +0100

    :sparkles: add new page and store data ids in api file

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const albums_ids = ["63MQldklfxkjYDoUE4Tppz", "4FpJcNgOvIpSBeJgRg3OfN", "2UwqpfQtNuhBwviIC0f2ie"];
+const artists_ids = ["63MQldklfxkjYDoUE4Tppz", "2UwqpfQtNuhBwviIC0f2ie", "3QVolfxko2UyCOtexhVTli", "1dfeR4HaWDbWqFHLkxsg1d"];
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css';
+
+const LoginPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Login Page</h1>
+        </div>
+    )
+};
+
+export default LoginPage;
