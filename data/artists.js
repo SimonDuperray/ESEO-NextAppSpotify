7bf13ea91082200520c2605b087595efa1972f71
Simon Duperray <simon.duperray@reseau.eseo.fr>
2022-11-09 18:59:18 +0100

    :sparkles: add data in local file

---
+++ data/artists.js
@@ -0,0 +1,158 @@
+export const artists = [
+    {
+        "external_urls": {
+            "spotify": "https://open.spotify.com/artist/63MQldklfxkjYDoUE4Tppz"
+        },
+        "followers": {
+            "href": null,
+            "total": 2126843
+        },
+        "genres": [
+            "french shoegaze",
+            "french synthpop",
+            "indietronica",
+            "metropopolis",
+            "neo-synthpop"
+        ],
+        "href": "https://api.spotify.com/v1/artists/63MQldklfxkjYDoUE4Tppz",
+        "id": "63MQldklfxkjYDoUE4Tppz",
+        "images": [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/ab6761610000e5eb5f5afb81942abd41d32dc2b5",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/ab676161000051745f5afb81942abd41d32dc2b5",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/ab6761610000f1785f5afb81942abd41d32dc2b5",
+                "width": 160
+            }
+        ],
+        "name": "M83",
+        "popularity": 69,
+        "type": "artist",
+        "uri": "spotify:artist:63MQldklfxkjYDoUE4Tppz"
+    },
+    {
+        "external_urls": {
+            "spotify": "https://open.spotify.com/artist/2UwqpfQtNuhBwviIC0f2ie"
+        },
+        "followers": {
+            "href": null,
+            "total": 3967896
+        },
+        "genres": [
+            "belgian hip hop",
+            "french hip hop",
+            "pop urbaine",
+            "rap belge",
+            "rap conscient"
+        ],
+        "href": "https://api.spotify.com/v1/artists/2UwqpfQtNuhBwviIC0f2ie",
+        "id": "2UwqpfQtNuhBwviIC0f2ie",
+        "images": [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/ab6761610000e5eb6d1dac9cd0d24eee082f5410",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/ab676161000051746d1dac9cd0d24eee082f5410",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/ab6761610000f1786d1dac9cd0d24eee082f5410",
+                "width": 160
+            }
+        ],
+        "name": "Damso",
+        "popularity": 74,
+        "type": "artist",
+        "uri": "spotify:artist:2UwqpfQtNuhBwviIC0f2ie"
+    },
+    {
+        "external_urls": {
+            "spotify": "https://open.spotify.com/artist/3QVolfxko2UyCOtexhVTli"
+        },
+        "followers": {
+            "href": null,
+            "total": 2454812
+        },
+        "genres": [
+            "belgian pop",
+            "variete francaise"
+        ],
+        "href": "https://api.spotify.com/v1/artists/3QVolfxko2UyCOtexhVTli",
+        "id": "3QVolfxko2UyCOtexhVTli",
+        "images": [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/ab6761610000e5eb18dab2c6c783248a7dc1392b",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/ab6761610000517418dab2c6c783248a7dc1392b",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/ab6761610000f17818dab2c6c783248a7dc1392b",
+                "width": 160
+            }
+        ],
+        "name": "Angèle",
+        "popularity": 68,
+        "type": "artist",
+        "uri": "spotify:artist:3QVolfxko2UyCOtexhVTli"
+    },
+    {
+        "external_urls": {
+            "spotify": "https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d"
+        },
+        "followers": {
+            "href": null,
+            "total": 43465969
+        },
+        "genres": [
+            "classic rock",
+            "glam rock",
+            "rock"
+        ],
+        "href": "https://api.spotify.com/v1/artists/1dfeR4HaWDbWqFHLkxsg1d",
+        "id": "1dfeR4HaWDbWqFHLkxsg1d",
+        "images": [
+            {
+                "height": 806,
+                "url": "https://i.scdn.co/image/b040846ceba13c3e9c125d68389491094e7f2982",
+                "width": 999
+            },
+            {
+                "height": 516,
+                "url": "https://i.scdn.co/image/af2b8e57f6d7b5d43a616bd1e27ba552cd8bfd42",
+                "width": 640
+            },
+            {
+                "height": 161,
+                "url": "https://i.scdn.co/image/c06971e9ff81696699b829484e3be165f4e64368",
+                "width": 200
+            },
+            {
+                "height": 52,
+                "url": "https://i.scdn.co/image/6dd0ffd270903d1884edf9058c49f58b03db893d",
+                "width": 64
+            }
+        ],
+        "name": "Queen",
+        "popularity": 83,
+        "type": "artist",
+        "uri": "spotify:artist:1dfeR4HaWDbWqFHLkxsg1d"
+    }
+]
