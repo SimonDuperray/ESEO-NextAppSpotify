321f720dea8ca6a2067d8200ef8551cec91aa240
Simon Duperray <simon.duperray@reseau.eseo.fr>
2022-12-14 14:48:13 +0100

    :sparkles: print picked metrics dynamically

--- pages/recommendations.js
+++ pages/recommendations.js
@@ -4,15 +4,50 @@ import Link from "next/link";
 import {HomeOutlined} from "@ant-design/icons";
 import {outMetrics} from "../data/metrics";
 import RecommendationMetricCard from "../components/cards/RecommendationMetricCard";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {pick} from "next/dist/lib/pick";
 
-const recommendations = () => {
+const Recommendations = () => {
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [pickedMetrics, setPickedMetrics] = useState([]);
 
+    useEffect(() => {
+        console.log(`PickedMetrics init: ${JSON.stringify(pickedMetrics)}`);
+    }, []);
+
+    const renderMetricsToDisplay = () => {
+        let toRet = "> ";
+        if(pickedMetrics.length > 0) {
+            for(let i = 0; i < pickedMetrics.length; i++) {
+                if(i === 0){
+                    toRet += pickedMetrics[i];
+                } else {
+                    toRet += " - " + pickedMetrics[i];
+                }
+            }
+        } else {
+            toRet = "[No Metrics Picked]";
+        }
+        return toRet;
+    }
+
     const handlePickedMetricsState = (event, met) => {
-        console.log(`just picked ${met} metric!`);
+        event.preventDefault();
+        let currentState = pickedMetrics;
+        if(currentState.includes(met)) {
+            // if the picked met is already on the state
+            let idx = currentState.indexOf(met);
+            if(idx > -1) {
+                currentState.splice(idx, 1);
+            }
+        } else {
+            // if the picked met is not on the state
+            currentState.push(met);
+        }
+        setPickedMetrics(currentState);
+        console.log(`just picked ${met} metric - state updated!`);
+        console.log(JSON.stringify(pickedMetrics));
+        document.querySelector('#metrics-tag').innerHTML = renderMetricsToDisplay();
     }
 
     return (
@@ -35,18 +70,23 @@ const recommendations = () => {
                                     onClick={(e) => handlePickedMetricsState(e, met)}
                                     key={met}
                                 >
-                                    <RecommendationMetricCard label={met}/>
+                                    <RecommendationMetricCard
+                                        label={met}
+                                    />
                                 </div>
                             )
                         })
                     }
                 </div>
                 <hr />
-                <h3>According to your preferences, here are the tracks you may appreciate:</h3>
+                <h3>
+                    You can find just below some tracks you may appreciate based on your preferences:
+                </h3>
+                <h4 id="metrics-tag"></h4>
             </div>
             <Footer />
         </div>
     );
 };
 
-export default recommendations;
+export default Recommendations;
